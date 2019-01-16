0ed281a37f71ff8ac99607d789edf17d0d1f9bfe
Jascha <jascha@jaeh.at>
2019-01-16 17:49:03 +0100

    update jellyfish

--- public/2018/radouan-zeghidour-no-future-jellyfish/nfjmap/scripts/nfj.js
+++ public/2018/radouan-zeghidour-no-future-jellyfish/nfjmap/scripts/nfj.js
@@ -22,10 +22,11 @@ function init () {
   // set the view in the middle of the image
   map.setView(rc.unproject([img[0] / 2 + 600, img[1] / 2 + 200]), 6);
 
-
   // the tile layer containing the image generated with gdal2tiles --leaflet ...
   L.tileLayer('/2018/radouan-zeghidour-no-future-jellyfish/nfjmap/tilesjpg/{z}/{x}/{y}.jpg', {
     noWrap: true,
     attribution: 'asdf'
   }).addTo(map);
 }
+
+document.body.onload = init
